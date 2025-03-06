7a2c3719bbbd63341ed6c5253269dbce4fdeb68a
Akash Patel <117297200+luminarakash@users.noreply.github.com>
2025-03-06 22:10:15 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+import React, {useState, useMemo} from 'react'
+import styled from "styled-components";
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from './Components/Navigation/Navigation'
+import Dashboard from './Components/Dashboard/Dashboard';
+import Income from './Components/Income/Income'
+import Expenses from './Components/Expenses/Expenses';
+import { useGlobalContext } from './context/globalContext';
+
+function App() {
+  const [active, setActive] = useState(1)
+
+  const global = useGlobalContext()
+  console.log(global);
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+
+  return (
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+        <main>
+          {displayData()}
+        </main>
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background: linear-gradient(to bottom, #6f96ff, #eaf6fc);
+  position: relative;
+  main{
+    flex: 1;
+    background: linear-gradient(to top, #6f96ff, #eaf6fc);
+
+    border: 2px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 10px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
+export default App;
